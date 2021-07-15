89b8c9ac70e00e43c28e21c0be868498e07b1392
Ryuya-rylynn <78291307+Ryuya-rylynn@users.noreply.github.com>
2021-07-16 03:26:47 +0900

    userモデルにバリデーション追加。また、userコントローラーにアクションを定義。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-end
+  validates :name, presence: true, length: {maximum:8}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :password_digest, presence: true, length: {minimum:8, maximum:32}
+end32
