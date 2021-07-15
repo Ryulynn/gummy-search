60b2bd6561f617a19e29cfbda23053701a0f9a17
Ryuya-rylynn <78291307+Ryuya-rylynn@users.noreply.github.com>
2021-07-15 17:53:06 +0900

    簡易的にヘッダーを作成。また、ルーティングでhome#topをrootパスに設定。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/sample', to: 'samples#index'
-  get '/', to: 'home#top'
+  root to: 'home#top'
   get '/about', to: 'home#about'
 end
