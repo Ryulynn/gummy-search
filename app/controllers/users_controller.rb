f0046016a50364ab58e8ff9fe9c2098d2d54d11e
Ryuya-rylynn <78291307+Ryuya-rylynn@users.noreply.github.com>
2021-07-16 22:55:51 +0900

    ユーザー登録機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,19 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "アカウントを作成しました"
+      session[:user_name] = @user.name
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render new_user_path
+    end
   end
 
   def edit
@@ -18,5 +28,10 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,4 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum:8}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  validates :password_digest, presence: true, length: {minimum:8, maximum:32}
-end32
+end
