c49b41678e98570fa2c99f06687dfcdbd57655b9
Ryuya-rylynn <78291307+Ryuya-rylynn@users.noreply.github.com>
2021-07-17 23:47:30 +0900

    ApplicationControllerにSessionsHelperをインクルード。また、今後作成するログインが必要なページへの未ログインアクセスを制御するために、privateメソッドを定義。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+
+  private
+
+  def logged_in_user
+    unless logged_in?
+      flash[:notice] = "ログインが必要です"
+      redirect_to login_path
+    end
+  end
 end
