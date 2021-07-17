7c3a1fd855866ee783ea393f623edd10e6376bd8
Ryuya-rylynn <78291307+Ryuya-rylynn@users.noreply.github.com>
2021-07-17 23:44:08 +0900

    ログイン機能に関するビューとコントローラーとヘルパーを作成

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,24 @@
 module SessionsHelper
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    if session[:user_id]
+      session[:user_name] ||= User.find_by(id: session[:user_id]).name
+    end
+  end
+
+  def current_user?(user)
+    user.name == current_user
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def log_out
+    session.delete(:user_id)
+    session.delete(:user_name)
+  end
 end
