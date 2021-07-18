756d9952eae0bb204f21978f0591aa5925744106
Ryuya-rylynn <78291307+Ryuya-rylynn@users.noreply.github.com>
2021-07-19 00:11:58 +0900

    usersコントローラにshow,edit,update,destroyアクションを定義し、各ビューを作成。また、不正アクセスを禁止するためのbefore_actionを定義、設定した。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,18 @@ class ApplicationController < ActionController::Base
       redirect_to login_path
     end
   end
+
+  def not_logged_in_user
+    if logged_in?
+      flash[:notice] = "既にログインしています"
+      redirect_to user_path(session[:user_id])
+    end
+  end
+
+  def correct_account?
+    unless current_user?(User.find_by(id: params[:id]))
+      flash[:notice] = "不正なアクセスです"
+      redirect_to root_path
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,7 @@
 class UsersController < ApplicationController
-  def index
-  end
-
-  def show
-  end
+  before_action :logged_in_user, only:[:edit, :show, :update, :destroy]
+  before_action :correct_account?, only:[:edit, :show, :update, :destroy]
+  before_action :not_logged_in_user, only:[:new]
 
   def new
     @user = User.new
@@ -17,22 +15,44 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to root_path
     else
-      render new_user_path
+      render "new"
     end
   end
 
   def edit
+    set_user
+  end
+
+  def show
+    set_user
   end
 
   def update
+    set_user
+    if @user.update(user_params)
+      flash[:notice] = "登録内容を変更しました"
+      session[:user_name] = @user.name
+      redirect_to user_path(session[:user_id])
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    set_user
+    @user.destroy
+    flash[:notice] = "アカウントを削除しました"
+    log_out
+    redirect_to root_path
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
+  end
+
+  def set_user
+    @user = User.find_by(id: params[:id])
   end
 end
