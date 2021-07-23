84349e0470a28fadb4b4d4b8329012cc8c54c427
Ryuya-rylynn <78291307+Ryuya-rylynn@users.noreply.github.com>
2021-07-24 01:28:30 +0900

    session helperのテストを作成

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -1,15 +1,64 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the SessionsHelper. For example:
-#
-# describe SessionsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe SessionsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:user2) { create(:user) }
+
+  before do
+    log_in(user)
+  end
+
+  it "log_inメソッドのテスト" do
+    expect(session[:user_id]).to eq user.id
+  end
+
+  it "current_userメソッドのテスト" do
+    expect(helper.current_user).to eq user.name
+    expect(session[:user_name]).to eq user.name
+  end
+
+  it "current_user_imageメソッドのテスト" do
+    expect(helper.current_user_image.identifier).to eq user.image.identifier
+  end
+
+  describe "current_user?メソッドのテスト" do
+    context "存在しないユーザーの入力時" do
+      it "falseを返す" do
+        expect(current_user?(User.find_by(name: "none_data"))).to eq false
+      end
+    end
+
+    context "存在するユーザーの入力時、current_userとuserが異なる場合" do
+      it "falseを返す" do
+        expect(current_user?(user2)).to eq false
+      end
+    end
+
+    context "存在するユーザーの入力時、current_userとuserが一致する場合" do
+      it "trueを返す" do
+        expect(current_user?(user)).to eq true
+      end
+    end
+  end
+
+  describe "logged_in?メソッドのテスト" do
+    context "ログインしている場合" do
+      it "trueを返す" do
+        expect(logged_in?).to eq true
+      end
+    end
+
+    context "ログインしていない場合" do
+      it "falseを返す" do
+        log_out
+        expect(logged_in?).to eq false
+      end
+    end
+  end
+
+  it "log_outメソッドのテスト" do
+    log_out
+    expect(session[:user_id]).to eq nil
+    expect(session[:user_name]).to eq nil
+  end
 end
