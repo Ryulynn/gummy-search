c1babe4715764da1f0253bb7ab0a2a06fd5df77a
Ryuya-rylynn <78291307+Ryuya-rylynn@users.noreply.github.com>
2021-07-25 22:06:19 +0900

    Gummyモデルにcarrierwaveの設定を追加

--- app/models/gummy.rb
+++ app/models/gummy.rb
@@ -2,4 +2,5 @@ class Gummy < ApplicationRecord
   validates :name, presence: true
   validates :image, presence: true
   validates :flavor_id_1, presence: true
+  mount_uploader :image, UserImageUploader
 end
