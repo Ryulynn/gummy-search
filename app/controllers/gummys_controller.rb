832d9271938c6cc6f56cf5ebca33c33f9b548a2c
Ryuya-rylynn <78291307+Ryuya-rylynn@users.noreply.github.com>
2021-07-25 18:28:37 +0900

    flavorモデルを作成(rails g model Flavor ...)

--- app/controllers/gummys_controller.rb
+++ app/controllers/gummys_controller.rb
@@ -1,4 +1,31 @@
 class GummysController < ApplicationController
+  def index
+    @gummies = Gummy.all
+  end
+
   def new
+    @gummy = Gummy.new
+  end
+
+  def create
+    @gummy = Gummy.new(gummy_params)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:gummy).permit(:name, :image, :flavor_id_1, :flavor_id_2, :flavor_id_3, :flavor_id_4)
   end
 end
---
+++ spec/factories/flavors.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :flavor do
+    name { "MyString" }
+  end
+end
