e4b05cab2cc96bc74c28decb659d31fd56bef0d1
Ryuya-rylynn <78291307+Ryuya-rylynn@users.noreply.github.com>
2021-07-27 18:06:43 +0900

    reviewsヘルパーの設定

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,15 @@
 module ReviewsHelper
+  def reviewed(current_user_id, current_gummy_id)
+    if Review.where(user_id: current_user_id, gummy_id: current_gummy_id) != []
+      true
+    end
+  end
+
+  def your_review
+    Review.find_by(user_id: session[:user_id], gummy_id: params[:gummy])
+  end
+
+  def correct_reviewer(params_user_id, current_user_id)
+    params_user_id.to_i == current_user_id.to_i
+  end
 end
