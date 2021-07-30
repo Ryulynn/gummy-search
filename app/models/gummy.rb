6dc23c4b14ba8fcd4f9cb429929fb2f21b94f84b
Ryuya-rylynn <78291307+Ryuya-rylynn@users.noreply.github.com>
2021-07-30 12:19:20 +0900

    モデルにバリデーションを追加

--- app/models/gummy.rb
+++ app/models/gummy.rb
@@ -3,5 +3,6 @@ class Gummy < ApplicationRecord
   validates :name, presence: true
   validates :image, presence: true
   validates :flavor_id_1, presence: true
+  validates :maker_id, presence: true
   mount_uploader :image, UserImageUploader
 end
