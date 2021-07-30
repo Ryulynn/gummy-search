23d650eb8568013ea7aa2f20b00c037bbc03ba6d
Ryuya-rylynn <78291307+Ryuya-rylynn@users.noreply.github.com>
2021-07-30 13:11:09 +0900

    makersコントローラーとビューファイルの記述

--- app/controllers/makers_controller.rb
+++ app/controllers/makers_controller.rb
@@ -1,4 +1,25 @@
 class MakersController < ApplicationController
+  before_action :logged_in_user, only: [:new]
+
   def new
+    @makers = Maker.all
+    @maker = Maker.new
+  end
+
+  def create
+    @maker = Maker.new(maker_params)
+    if @maker.save
+      flash[:notice] = "メーカーを登録しました"
+      redirect_to new_maker_path
+    else
+      flash[:notice] = "登録できませんでした。入力内容に誤りがあります。"
+      redirect_to new_maker_path
+    end
+  end
+
+  private
+
+  def maker_params
+    params.require(:maker).permit(:name)
   end
 end
