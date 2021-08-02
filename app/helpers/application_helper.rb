2025d7314c7bd7618a25843997e68936915947d4
Ryuya-rylynn <78291307+Ryuya-rylynn@users.noreply.github.com>
2021-08-03 02:45:25 +0900

    admin_userメソッドをsessionヘルパーからapplicationヘルパーに移動。それに伴いテストの作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def correct_poster(params_user_id, current_user_id)
     params_user_id.to_i == current_user_id.to_i
   end
+
+  def admin_user
+    unless User.find_by(id: session[:user_id]).nil?
+      User.find_by(id: session[:user_id]).admin
+    end
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe ReviewsHelper, type: :helper do
   let(:user) { create(:user) }
   let(:user2) { create(:user) }
+  let(:user_admin) { create(:user, admin: true) }
 
   describe "correct_posterメソッドのテスト" do
     context "入力されたユーザーidとログインしているユーザーidが一致する場合" do
@@ -14,6 +15,34 @@ RSpec.describe ReviewsHelper, type: :helper do
     context "入力されたユーザーidとログインしているユーザーidが一致しない場合" do
       it "falseを返すこと" do
         expect(helper.correct_poster(user2.id, user.id)).to eq false
+      end
+    end
+  end
+
+  describe "admin_userメソッドのテスト" do
+    context "ログインしていない場合" do
+      it "nilを返すこと" do
+        expect(helper.admin_user).to eq nil
+      end
+    end
+
+    context "通常ユーザーでログインしている場合" do
+      before do
+        allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return({ user_password: user.password, user_id: user.id })
+      end
+
+      it "falseを返すこと" do
+        expect(helper.admin_user).to eq false
+      end
+    end
+
+    context "アドミンユーザーでログインしている場合" do
+      before do
+        allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return({ user_password: user_admin.password, user_id: user_admin.id })
+      end
+
+      it "trueを返すこと" do
+        expect(helper.admin_user).to eq true
       end
     end
   end
