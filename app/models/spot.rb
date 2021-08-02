3a6a2abf147932f8b800cae776d419e00a4d94dd
Ryuya-rylynn <78291307+Ryuya-rylynn@users.noreply.github.com>
2021-08-02 16:44:01 +0900

    spotモデルにバリデーションと、geocoderの設定を追加。

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,4 +1,8 @@
 class Spot < ApplicationRecord
   belongs_to :user
   belongs_to :gummy
+  validates :shop, presence: true
+  validates :address, presence: true
+  geocoded_by :address
+  after_validation :geocode
 end
