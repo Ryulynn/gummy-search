1858d74c1202b76b808f5121a8dbdae75a26177f
Ryuya-rylynn <78291307+Ryuya-rylynn@users.noreply.github.com>
2021-08-03 02:59:01 +0900

    admin機能に関するテストを追加

--- spec/features/maker_spec.rb
+++ spec/features/maker_spec.rb
@@ -2,6 +2,27 @@ require 'rails_helper'
 
 RSpec.feature "maker機能のfeatureテスト", type: :feature do
   let(:user) { create(:user) }
+  let(:user_admin) { create(:user, admin: true) }
+  let!(:maker) { create(:maker) }
+
+  feature "makers#index" do
+    background do
+      visit login_path
+      fill_in 'session-name-form', with: "#{user_admin.email}"
+      fill_in 'session-password-form', with: "#{user_admin.password}"
+      click_on "Log in"
+      visit makers_path
+    end
+
+    scenario "メーカーが表示されていること" do
+      expect(page).to have_content maker.name
+    end
+
+    scenario "編集ボタンを押すと、対応したメーカー編集ページにアクセスすること" do
+      click_on 'edit-maker-link-0' # idで指定
+      expect(current_path).to eq edit_maker_path(maker.id)
+    end
+  end
 
   feature "makers#new" do
     background do
@@ -12,10 +33,32 @@ RSpec.feature "maker機能のfeatureテスト", type: :feature do
       visit new_maker_path
     end
 
-    scenario "フレーバーが登録できること" do
+    scenario "メーカーが登録できること" do
       fill_in 'new-maker-name-form', with: "test_maker"
       click_on 'new-maker-register-button'
       expect(Maker.last.name).to eq "test_maker"
+    end
+  end
+
+  feature "makers#edit" do
+    background do
+      visit login_path
+      fill_in 'session-name-form', with: "#{user_admin.email}"
+      fill_in 'session-password-form', with: "#{user_admin.password}"
+      click_on "Log in"
+      visit edit_maker_path(maker.id)
+    end
+
+    scenario "メーカーを編集できること" do
+      fill_in 'edit-maker-form', with: "edit_maker"
+      click_on 'edit-maker-change-button' # idで指定
+      maker_name = Maker.find_by(id: maker.id).name
+      expect(maker_name).to eq "edit_maker"
+    end
+
+    scenario "メーカーを削除できること" do
+      click_on "削除"
+      expect(Maker.find_by(id: maker.id)).to eq nil
     end
   end
 end
--- spec/requests/gummies_spec.rb
+++ spec/requests/gummies_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Gummies", type: :request do
   let(:user) { create(:user) }
+  let(:user_admin) { create(:user, admin: true) }
   let(:flavor) { create(:flavor) }
   let(:maker) { create(:maker) }
   let(:gummy) { create(:gummy, :skip_validate, flavor_id_1: flavor.id, maker_id: maker.id) }
@@ -50,6 +51,48 @@ RSpec.describe "Gummies", type: :request do
 
     it "httpステータスが200を返すこと" do
       expect(response).to have_http_status "200"
+    end
+  end
+
+  describe "GET /edit" do
+    context "ログインしていない場合" do
+      before do
+        get edit_gummy_path(gummy.id)
+      end
+
+      it "ログイン画面にリダイレクトすること" do
+        expect(response).to redirect_to login_path
+      end
+
+      it "「ログインが必要です」と表示されること" do
+        expect(flash[:notice]).to match('ログインが必要です')
+      end
+    end
+
+    context "通常ユーザーでログインしている場合" do
+      before do
+        allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return({ user_password: user.password, user_id: user.id })
+        get edit_gummy_path(gummy.id)
+      end
+
+      it "ホームページにリダイレクトすること" do
+        expect(response).to redirect_to root_path
+      end
+
+      it "「不正なアクセスです」と表示されること" do
+        expect(flash[:notice]).to match('不正なアクセスです')
+      end
+    end
+
+    context "アドミンユーザーでログインしている場合" do
+      before do
+        allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return({ user_password: user_admin.password, user_id: user_admin.id })
+        get edit_gummy_path(gummy.id)
+      end
+
+      it "httpステータスが200を返すこと" do
+        expect(response).to have_http_status "200"
+      end
     end
   end
 
