294819991e74e2ef10da24fdc31ba1448654f253
Ryuya-rylynn <78291307+Ryuya-rylynn@users.noreply.github.com>
2021-08-05 03:27:09 +0900

    home#aboutのビュートテストを削除

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -10,14 +10,4 @@ RSpec.describe "Homes", type: :request do
       expect(response).to have_http_status "200"
     end
   end
-
-  describe "GET /about" do
-    before do
-      get about_path
-    end
-
-    it "httpステータスが200を返すこと" do
-      expect(response).to have_http_status "200"
-    end
-  end
 end
