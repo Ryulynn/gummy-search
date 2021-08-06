fc3404485ff9c6e517a40d3eaa68813379f93097
Ryuya-rylynn <78291307+Ryuya-rylynn@users.noreply.github.com>
2021-08-06 17:59:34 +0900

    users#mapアクションにおいて、各グミごとに投稿した位置情報を表示するようにビューとコントローラーを変更。また、それに伴いヘルパーメソッドの作成。さらに、変更をテストするspecファイルの記述を追加。

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,8 @@ module UsersHelper
   def posted_spot_gummy_flavor(spot)
     Flavor.find_by(id: spot.gummy.flavor_id_1).name
   end
+
+  def posted_gummy_spot_by_you(gummy_id, user_id)
+    Spot.where(gummy_id: gummy_id, user_id: user_id)
+  end
 end
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -4,9 +4,29 @@ RSpec.describe UsersHelper, type: :helper do
   let(:user) { create(:user) }
   let(:flavor) { create(:flavor) }
   let(:gummy) { create(:gummy, :skip_validate, flavor_id_1: flavor.id) }
+  let(:gummy_not_posted_spot) { create(:gummy, :skip_validate) }
   let(:review) { create(:review, user_id: user.id, gummy_id: gummy.id) }
+  let(:spot) { create(:spot, user_id: user.id, gummy_id: gummy.id) }
 
   it "reviewd_gummy_flavorメソッドのテスト" do
     expect(helper.reviewed_gummy_flavor(review)).to eq flavor.name
+  end
+
+  it "posted_spot_gummy_flavorメソッドのテスト" do
+    expect(helper.posted_spot_gummy_flavor(spot)).to eq flavor.name
+  end
+
+  describe "posted_gummy_spot_by_youメソッドのテスト" do
+    context "ユーザーが、あるグミの位置情報を投稿していない場合" do
+      it "空配列[]を返すこと" do
+        expect(helper.posted_gummy_spot_by_you(gummy_not_posted_spot.id, user.id)).to eq []
+      end
+    end
+
+    context "ユーザーが、あるグミの位置情報を投稿している場合" do
+      it "spotのデータを返すこと" do
+        expect(helper.posted_gummy_spot_by_you(gummy.id, user.id)).to eq [spot]
+      end
+    end
   end
 end
